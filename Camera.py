cdbfd63480d7293dc42183befd5335f290b8d841
Deniel-b <kuravin-denis@yandex.ru>
2024-12-03 22:25:57 +0300

    added Camera object in main.py in main window. Trying add QVideoSink to processing video in AI

--- Camera.py
+++ Camera.py
@@ -1,16 +1,35 @@
 from PySide6.QtWidgets import (QWidget)
-from PySide6.QtMultimedia import (QCamera, QMediaCaptureSession, QMediaDevices, QVideoSink)
+from PySide6.QtMultimedia import (QCamera, QMediaCaptureSession, QVideoSink, QCameraFormat,
+                                  QImageCapture, QVideoFrame)
 from PySide6.QtMultimediaWidgets import QVideoWidget
+from PySide6.QtCore import Slot, Signal, QObject
 
 
 class Camera(QWidget):
     def __init__(self):
         super().__init__()
 
+        self._format = QCameraFormat()
+        self._format.minFrameRate = 1.0
+        self._format.maxFrameRate = 5.0
+        self._format.resolution = (1280, 720)
+
         self._capture_session = QMediaCaptureSession()
         self._camera = QCamera()
+        self._camera.setCameraFormat(self._format)
+
         self._capture_session.setCamera(self._camera)
+
+        self._image_capture = QImageCapture(self._camera)
+
         self.viewfinder = QVideoWidget()
+
+        self._sink = QVideoSink()
+        self._sink.connect(self.frame_changed, QVideoSink.videoFrameChanged)
+
+        self._capture_session.setVideoSink(self._sink)
         self._capture_session.setVideoOutput(self.viewfinder)
+
         self.viewfinder.show()
         self._camera.start()
+
--- camerasCheck.py
+++ camerasCheck.py
@@ -1,3 +1,5 @@
+import datetime
+
 from PyCameraList.camera_device import test_list_cameras, list_video_devices, list_audio_devices
 from PySide6.QtMultimedia import (QCamera, QImageCapture,
                                   QCameraDevice, QMediaCaptureSession,
@@ -6,3 +8,5 @@ import pprint
 
 available_cameras = QMediaDevices.videoInputs()
 print(available_cameras[0])
+
+print(str(datetime.datetime.now()))
